7493e36c050a8feaef5c433b08200abdc424e437
unknown <ibrahimkhalil.mine@gmail.com>
2022-05-14 12:40:03 +0600

    navbar added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,5 +5,20 @@ module.exports = {
   theme: {
     extend: {},
   },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#0FCFEC",
+          secondary: "#19D3AE",
+          accent: "#19D3AE",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
   plugins: [require("daisyui")],
 }
