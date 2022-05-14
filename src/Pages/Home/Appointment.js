44bdde5f10696301613cd9eb8cc1adeb71388d18
unknown <ibrahimkhalil.mine@gmail.com>
2022-05-14 16:52:44 +0600

    appoinment

---
+++ src/Pages/Home/Appointment.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import doctor from '../../assets/images/doctor.png'
+import appointment from '../../assets/images/appointment.png'
+import Button from '../Shared/Button';
+const Appointment = () => {
+    return (
+        <div className='mt-28'>
+            <div className='flex' style={{ backgroundImage: `url(${appointment})` }}>
+                <div className='flex-1'>
+                    <img className='mt-[-125px]' src={doctor} alt="" />
+                </div>
+                <div className='text-white flex-1 flex items-center'>
+                    <div className='pr-10'>
+                        <h3 className='text-2xl text-secondary font-bold'>Appointment</h3>
+                        <h1 className='text-4xl py-6'>Make an appointment Today</h1>
+                        <p className='pb-6'>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsumis that it has a more-or-less normal distribution of letters,as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page</p>
+                        <Button></Button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Appointment;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Appointment from './Appointment';
 import Banner from './Banner';
 import Services from './Services';
 
@@ -7,6 +8,7 @@ const Home = () => {
         <div className='px-12'>
             <Banner></Banner>
             <Services></Services>
+            <Appointment></Appointment>
         </div>
     );
 };
