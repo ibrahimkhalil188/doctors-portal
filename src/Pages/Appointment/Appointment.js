d01d54e8a49687ef7962b02d097263fe5368d68e
unknown <ibrahimkhalil.mine@gmail.com>
2022-05-15 10:39:36 +0600

    added appointment

--- src/Pages/Appointment/Appointment.js
+++ src/Pages/Appointment/Appointment.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Footer from '../Shared/Footer';
 import AppointmentBanner from './AppointmentBanner';
+import AvailableAppointment from './AvailableAppointment';
 
 const Appointment = () => {
     return (
         <div>
             <AppointmentBanner></AppointmentBanner>
+            <AvailableAppointment></AvailableAppointment>
             <Footer></Footer>
         </div>
     );
--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,17 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import Button from '../Shared/Button';
 
 const AvailableAppointment = () => {
     const [appointmentServices, setAppointmentServices] = useState([])
     useEffect(() => {
-        fetch("https://raw.githubusercontent.com/ProgrammingHero1/doctors-portal-client-module-72/main/public/services.json")
-            .then(res => res.json)
+        fetch("Services.json")
+            .then(res => res.json())
             .then(data => setAppointmentServices(data))
-    })
+    }, [])
+    console.log(appointmentServices)
     return (
-        <div>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
             {appointmentServices.map(data =>
                 <div>
-                    { }
+                    <div class="card text-primary-content shadow-lg items-center">
+                        <div class="card-body">
+                            <h2 class="card-title">{data.name}</h2>
+                            <p>{data.slots[0] ? <span> {data.slots[0]}</span> : <span className='text-red-500'>Try another day</span>}</p>
+                            <p className='text-xl'>{data.slots.length} {data.slots.length ? "Slots" : "Slot"} available</p>
+
+                            <button disabled={data.slots.length === 0} class="btn btn-primary">Book appointment</button>
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
