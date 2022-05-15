a847436bae185ebc2dde84d7e9c1a559c336232c
unknown <ibrahimkhalil.mine@gmail.com>
2022-05-15 11:08:09 +0600

    modal added

--- src/Pages/Appointment/AvailableAppointment.js
+++ src/Pages/Appointment/AvailableAppointment.js
@@ -1,29 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import Button from '../Shared/Button';
+import BookingModal from './BookingModal';
 
-const AvailableAppointment = () => {
+const AvailableAppointment = ({ selected }) => {
+    const [treatment, setTreatment] = useState({})
     const [appointmentServices, setAppointmentServices] = useState([])
     useEffect(() => {
         fetch("Services.json")
             .then(res => res.json())
             .then(data => setAppointmentServices(data))
     }, [])
-    console.log(appointmentServices)
-    return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            {appointmentServices.map(data =>
-                <div>
-                    <div class="card text-primary-content shadow-lg items-center">
-                        <div class="card-body">
-                            <h2 class="card-title">{data.name}</h2>
-                            <p>{data.slots[0] ? <span> {data.slots[0]}</span> : <span className='text-red-500'>Try another day</span>}</p>
-                            <p className='text-xl'>{data.slots.length} {data.slots.length ? "Slots" : "Slot"} available</p>
 
-                            <button disabled={data.slots.length === 0} class="btn btn-primary">Book appointment</button>
+    return (
+        <div>
+            <h1>{selected}</h1>
+            <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
+                {appointmentServices.map(data =>
+                    <div key={data._id}>
+                        <div class="card text-primary-content shadow-lg items-center">
+                            <div class="card-body">
+                                <h2 class="card-title">{data.name}</h2>
+                                <p>{data.slots[0] ? <span> {data.slots[0]}</span> : <span className='text-red-500'>Try another day</span>}</p>
+                                <p className='text-xl'>{data.slots.length} {data.slots.length ? "Slots" : "Slot"} available</p>
+
+
+                                <label
+                                    onClick={() => setTreatment(data)}
+                                    disabled={data.slots.length === 0} for="my-modal-6" class="btn btn-primary">Book appointment</label>
+
+                                <BookingModal treatment={treatment}></BookingModal>
+                            </div>
                         </div>
                     </div>
-                </div>
-            )}
+                )
+                }
+            </div >
         </div>
     );
 };
